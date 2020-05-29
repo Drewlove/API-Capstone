2b0313d0ca556f330e8d3b8c3e69fda1812a3835
Drew Love <davidandrewlove84@gmail.com>
2020-05-29 12:56:16 -0400

    change landing page icon

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ $(renderLandingPage)
 function renderLandingPage() {
     let landingPage =
         `<section class='landing'>
-        <img alt='USA lights at night' src='rb.gy/ssymcb'>
+        <img alt='USA lights at night' src='https://lh3.googleusercontent.com/proxy/Mfnp8PNdC3ErZlIq2uEsbu9aiAhauvfZ9KhebFA3urrlwHaB9NQb6LnRUwG191Q9GhQvmVI3DiVEaiIMKoVDSyxJy8jADNi66zMYnLwxQt0H7f4xF0hRZQ'>
         <button class='light-button'>START</button>
     </section>`;
     $('main').append(landingPage)
